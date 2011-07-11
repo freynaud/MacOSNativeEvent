2f23c6928a59f8d47dee850fd449bbfa3f693079
Francois Reynaud <francois.reynaud@gmail.com>
2011-07-11 19:12:27 +0100

    Initial Commit

---
+++ MacOSNativeEvent/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MacOSNativeEvent
+//
+//  Created by François Reynaud on 11/07/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
